c5112df04bdbf5b2900c4b05840318e0ddfbb61f
SolRomero1999git config --global user.email <solromero051099@gmailcom>
2024-06-06 21:13:02 -0300

    solución error botón

--- src/objetos/BotonAmarrillo.js
+++ src/objetos/BotonAmarrillo.js
@@ -1,15 +1,23 @@
-export default class BotonAmarrillo {
-
+export default class BotonAmarillo {
     constructor(scene, x, y, texto, callback, escala = 1, tamanio = 24) {
-        this.texto = scene.add.text(0, 0, texto, { fontSize: tamanio, fontStyle: 'bold', color: 'white', fontFamily: 'AnyMale', stroke: 'black', strokeThickness: 6 }).setInteractive({ useHandCursor: true }).setOrigin(0.5);
+        this.texto = scene.add.text(0, 0, texto, { 
+            fontSize: `${tamanio}px`, 
+            fontStyle: 'bold', 
+            color: 'white', 
+            fontFamily: 'AnyMale', 
+            stroke: 'black', 
+            strokeThickness: 6 
+        }).setInteractive({ useHandCursor: true }).setOrigin(0.5);
 
         this.rectangle = scene.add.rectangle(0, 0, 320, 90, 0xffff00).setOrigin(0.5);
         this.rectangle.alpha = 0; // Set initial opacity to 0
+
         this.texto.on("pointerdown", () => {
             callback();
-        })
+        });
+
         this.texto.on("pointerover", () => {
-            this.rectangle.alpha = 0.8; // Set opacity to 1 when pointer is over
+            this.rectangle.alpha = 0.8; // Set opacity to 0.8 when pointer is over
         });
 
         this.texto.on("pointerout", () => {
